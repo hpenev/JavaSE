c4c9aeecf81f680f8474c6fb83d32eed2bbd6aeb
hpenev <hristo.penev@yahoo.com>
2017-06-22 00:25:33 +0300

    homework

--- Lesson9Recursion/src/loopsHomeworkWithRecursion/Task20.java
+++ Lesson9Recursion/src/loopsHomeworkWithRecursion/Task20.java
@@ -1,18 +1,72 @@
 package loopsHomeworkWithRecursion;
 
-public class Task20 {
-	public static void main(String[] args) {
+/*
+Задача 20:
+Да се състави програма, чрез която се извежда квадрат от
+цифри. Сумите от елементите на произволен ред или стълб са
+равни на 45.
+Пример:
+1 2 3 4 5 6 7 8 9 0
+2 3 4 5 6 7 8 9 0 1
+3 4 5 6 7 8 9 0 1 2
+4 5 6 7 8 9 0 1 2 3
+5 6 7 8 9 0 1 2 3 4
+6 7 8 9 0 1 2 3 4 5
+7 8 9 0 1 2 3 4 5 6
+8 9 0 1 2 3 4 5 6 7
+9 0 1 2 3 4 5 6 7 8
+0 1 2 3 4 5 6 7 8 9
+*/
 
-		for (int i = 1; i <= 10; i++) {
-			int num = i;
-			for (int j = 1; j <= 10; j++) {
-				if (num == 10) {
-					num = 0;
-				}
-				System.out.print(num + " ");
-				num++;
-			}
-			System.out.println();
-		}
+public class Task20 {
+    public static void main(String[] args) {
+
+	// iteration();
+	recursionRow(1, 10);
+    }
+
+    static void recursionRow(int start, int i) {
+	recursion(start, 10);
+
+	if (start == 10) {
+	    start = 0;
 	}
+
+	System.out.println(start + " ");
+	i--;
+
+	if (i == 0) {
+	    return;
+	}
+
+	recursionRow(start + 1, i);
+    }
+
+    static void recursion(int start, int i) {
+	if (start == 10) {
+	    start = 0;
+	}
+
+	System.out.print(start + " ");
+	i--;
+	if (i == 0) {
+	    return;
+	}
+	recursion(start + 1, i);
+
+    }
+
+    private static void iteration() {
+	for (int i = 1; i <= 10; i++) {
+	    int num = i;
+	    for (int j = 1; j <= 10; j++) {
+		if (num == 10) {
+		    num = 0;
+		}
+		System.out.print(num + " ");
+		num++;
+	    }
+	    System.out.println();
+	}
+    }
 }
--- Lesson9Recursion/src/loopsHomeworkWithRecursion/Task21.java
+++ Lesson9Recursion/src/loopsHomeworkWithRecursion/Task21.java
@@ -1,161 +1,180 @@
 package loopsHomeworkWithRecursion;
+/*
+Задача 21:
+Дадено е наредено тесте карти.
+Наредбата по тежест на карта е: 2,3,4,5,6,7,8,9,10, Вале, Дама,
+Поп, Асо.
+Наредбата по цвят на картите е: спатия, каро, купа, пика.
+Да се създаде програма, чрез която се въвежда N - число от
+интервала [1..51] и се извеждат въведения номер карта и
+останалите по-големи карти от тестето.
+Пример: 47.
+Изход: Поп купа, Поп пика, Асо спатия, Асо каро, Асо купа, Асо
+пика
+*/
 
 import java.util.Scanner;
 
 public class Task21 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a number [1..52]: ");
-		int cardNumber = sc.nextInt();
-		sc.close();
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter a number [1..52]: ");
+	int cardNumber = sc.nextInt();
+	sc.close();
 
-		if (cardNumber >= 1 && cardNumber <= 52) {
-			for (int i = cardNumber; i <= 52; i++) {
-				String color = "";
+	if (cardNumber >= 1 && cardNumber <= 52) {
 
-				int colorNumber = i % 4;
-				switch (colorNumber) {
-				case 0:
-					color = "пика";
-					break;
-				case 1:
-					color = "спатия";
-					break;
-				case 2:
-					color = "каро";
-					break;
-				case 3:
-					color = "купа";
-					break;
-				default:
-					color = "unknown color";
-					break;
-				}
+	    iteration(cardNumber);
 
-				int weightNumber = (i - 1) / 4;
-				String weight = "";
-
-				switch (weightNumber) {
-				case 0:
-					weight = "2";
-					break;
-				case 1:
-					weight = "3";
-					break;
-				case 2:
-					weight = "4";
-					break;
-				case 3:
-					weight = "5";
-					break;
-				case 4:
-					weight = "6";
-					break;
-				case 5:
-					weight = "7";
-					break;
-				case 6:
-					weight = "8";
-					break;
-				case 7:
-					weight = "9";
-					break;
-				case 8:
-					weight = "10";
-					break;
-				case 9:
-					weight = "Вале";
-					break;
-				case 10:
-					weight = "Дама";
-					break;
-				case 11:
-					weight = "Поп";
-					break;
-				case 12:
-					weight = "Асо";
-					break;
-				}
-
-				if (i != 52) {
-					System.out.printf("%s %s, ", weight, color);
-				} else {
-					System.out.printf("%s %s", weight, color);
-				}
-			}
-		} else {
-			System.out.println("Wrong input");
-		}
+	} else {
+	    System.out.println("Wrong input");
 	}
+    }
 
-	// private static String cardColor(int cardNumber) {
-	// String color = "";
-	//
-	// int colorNumber = cardNumber % 4;
-	// switch (colorNumber) {
-	// case 0:
-	// color = "пика";
-	// break;
-	// case 1:
-	// color = "спатия";
-	// break;
-	// case 2:
-	// color = "каро";
-	// break;
-	// case 3:
-	// color = "купа";
-	// break;
-	// }
-	// return color;
-	// }
-	//
-	// private static String cardWeight(int cardNumber) {
-	// int weightNumber = (cardNumber - 1) / 4;
-	// String weight = "";
-	//
-	// switch (weightNumber) {
-	// case 0:
-	// weight = "2";
-	// break;
-	// case 1:
-	// weight = "3";
-	// break;
-	// case 2:
-	// weight = "4";
-	// break;
-	// case 3:
-	// weight = "5";
-	// break;
-	// case 4:
-	// weight = "6";
-	// break;
-	// case 5:
-	// weight = "7";
-	// break;
-	// case 6:
-	// weight = "8";
-	// break;
-	// case 7:
-	// weight = "9";
-	// break;
-	// case 8:
-	// weight = "10";
-	// break;
-	// case 9:
-	// weight = "Вале";
-	// break;
-	// case 10:
-	// weight = "Дама";
-	// break;
-	// case 11:
-	// weight = "Поп";
-	// break;
-	// case 12:
-	// weight = "Асо";
-	// break;
-	// }
-	//
-	// return weight;
-	// }
+    private static void iteration(int cardNumber) {
+	for (int i = cardNumber; i <= 52; i++) {
+	    String color = "";
+
+	    int colorNumber = i % 4;
+	    switch (colorNumber) {
+	    case 0:
+		color = "пика";
+		break;
+	    case 1:
+		color = "спатия";
+		break;
+	    case 2:
+		color = "каро";
+		break;
+	    case 3:
+		color = "купа";
+		break;
+	    default:
+		color = "unknown color";
+		break;
+	    }
+
+	    int weightNumber = (i - 1) / 4;
+	    String weight = "";
+
+	    switch (weightNumber) {
+	    case 0:
+		weight = "2";
+		break;
+	    case 1:
+		weight = "3";
+		break;
+	    case 2:
+		weight = "4";
+		break;
+	    case 3:
+		weight = "5";
+		break;
+	    case 4:
+		weight = "6";
+		break;
+	    case 5:
+		weight = "7";
+		break;
+	    case 6:
+		weight = "8";
+		break;
+	    case 7:
+		weight = "9";
+		break;
+	    case 8:
+		weight = "10";
+		break;
+	    case 9:
+		weight = "Вале";
+		break;
+	    case 10:
+		weight = "Дама";
+		break;
+	    case 11:
+		weight = "Поп";
+		break;
+	    case 12:
+		weight = "Асо";
+		break;
+	    }
+
+	    if (i != 52) {
+		System.out.printf("%s %s, ", weight, color);
+	    } else {
+		System.out.printf("%s %s", weight, color);
+	    }
+	}
+    }
+
+    // private static String cardColor(int cardNumber) {
+    // String color = "";
+    //
+    // int colorNumber = cardNumber % 4;
+    // switch (colorNumber) {
+    // case 0:
+    // color = "пика";
+    // break;
+    // case 1:
+    // color = "спатия";
+    // break;
+    // case 2:
+    // color = "каро";
+    // break;
+    // case 3:
+    // color = "купа";
+    // break;
+    // }
+    // return color;
+    // }
+    //
+    // private static String cardWeight(int cardNumber) {
+    // int weightNumber = (cardNumber - 1) / 4;
+    // String weight = "";
+    //
+    // switch (weightNumber) {
+    // case 0:
+    // weight = "2";
+    // break;
+    // case 1:
+    // weight = "3";
+    // break;
+    // case 2:
+    // weight = "4";
+    // break;
+    // case 3:
+    // weight = "5";
+    // break;
+    // case 4:
+    // weight = "6";
+    // break;
+    // case 5:
+    // weight = "7";
+    // break;
+    // case 6:
+    // weight = "8";
+    // break;
+    // case 7:
+    // weight = "9";
+    // break;
+    // case 8:
+    // weight = "10";
+    // break;
+    // case 9:
+    // weight = "Вале";
+    // break;
+    // case 10:
+    // weight = "Дама";
+    // break;
+    // case 11:
+    // weight = "Поп";
+    // break;
+    // case 12:
+    // weight = "Асо";
+    // break;
+    // }
+    //
+    // return weight;
+    // }
 }
