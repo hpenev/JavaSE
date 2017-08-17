c96c1eb5d13db3bc981e5f93daa0be99e818aee5
hpenev <hristo.penev@yahoo.com>
2017-08-17 17:45:32 +0300

    Lesson for nested inner anonymous classes

---
+++ Lesson20-Nested_Inner_Anonymous_Classes/src/Demo.java
@@ -0,0 +1,10 @@
+
+public class Demo {
+    public static void main(String[] args) {
+	// Instantiating the outer class
+	Outer_Demo outer = new Outer_Demo();
+
+	// Accessing the display_Inner() method.
+	outer.display_Inner();
+    }
+}
