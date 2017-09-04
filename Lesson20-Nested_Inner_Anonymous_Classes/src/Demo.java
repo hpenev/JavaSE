5c9c3a048bf44240ba63fcae5adc8d9805f76e77
h.penev <hristo.penev@yahoo.com>
2017-09-04 16:28:09 +0300

    practice on inner/outer classes

--- Lesson20-Nested_Inner_Anonymous_Classes/src/Demo.java
+++ Lesson20-Nested_Inner_Anonymous_Classes/src/Demo.java
@@ -1,4 +1,3 @@
-
 public class Demo {
     public static void main(String[] args) {
 	// Instantiating the outer class
@@ -6,5 +5,8 @@ public class Demo {
 
 	// Accessing the display_Inner() method.
 	outer.display_Inner();
+
+	Outer_Demo.Static_Inner_Demo static_Inner_Demo = new Outer_Demo.Static_Inner_Demo();
+	static_Inner_Demo.print();
     }
 }
--- Lesson20-Nested_Inner_Anonymous_Classes/src/Outer_Demo.java
+++ Lesson20-Nested_Inner_Anonymous_Classes/src/Outer_Demo.java
@@ -1,12 +1,27 @@
 class Outer_Demo {
-    int num;
+    private int num;
+    private static int stat;
 
     // inner class
     private class Inner_Demo {
 	public void print() {
+	    System.out.println(stat);
 	    System.out.println("This is an inner class");
 	}
     }
+
+    public static class Static_Inner_Demo {
+	public void print() {
+	    System.out.println(stat);
+	    System.out.println("This is an static nested class");
+	}
+    }
+
+    Static_Inner_Demo s = new Static_Inner_Demo() {
+	public void print() {
+	    System.out.println("This is anonimus class");
+	};
+    };
 
     // Accessing he inner class from the method within
     void display_Inner() {
